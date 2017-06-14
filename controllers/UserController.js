f6067563f4dd2de72dfb7f861616e9c22c4acb62
web.ibrooks <gm.wroman@gmail.com>
2017-06-14 21:39:33 +0300

    Register route
    Added registration form, new npm modules are connected

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,18 @@
+let User = require('../models/User.js');
 module.exports = (app) => {
   app.route('/')
     .get(function(req, res){
       res.sendStatus(200);
+    })
+  app.route('/r')
+    .get(function(req, res){
+      //Рендер страницы регистрации
+      console.log('/r')
+      res.render('register');
+    })
+    .post(function(req, res){
+      let login = req.body.login;
+      let password = req.body.password;
+      
     })
 }
--- index.js
+++ index.js
@@ -2,13 +2,23 @@ let express = require('express'), //Подключение express
     app = express();
 
 let exhb = require('express-handlebars'), //Подключение основных NPM модулей
-    sessions = require('express-session'),
+    session = require('express-session'),
     mysql = require('mysql'),
-    io = require('socket.io');
+    io = require('socket.io'),
+    bodyParser = require('body-parser');
 
 let config = require('./config.js'),  //Подключение локальных модулей
     UserController = require('./controllers/UserController.js');
+
+app.use(session({ secret: 'GkGjsYUdfLdsB', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', exhb({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 UserController(app);
+
 app.listen(config.port, (err) => {
   err ? console.log(`Ошибка`) : console.log(`Сервер запущен на ${config.port} порту`)
 })
